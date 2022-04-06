56e038036e9a186aab8b664547ecfb5b06b9bd4e
spatrickliv <41328446+spatrickliv@users.noreply.github.com>
2022-04-06 14:05:46 +0100

    Fixing Eve's horribleness

--- Test_script_Away_Day.R
+++ Test_script_Away_Day.R
@@ -3,9 +3,11 @@
 lapply(c("ggplot2"),library, character.only = TRUE)
 
 # Read in the data
+
 parentaldata <- read.table(file = "speciesdata.txt", header = TRUE, sep = "\t")
 
 #subset for only the data we need
+
 parentaldata2 <- subset(parentaldata, 
                         (!is.na(parentaldata$FeedingOffspring)) & 
                         (!is.na(parentaldata$Incubation)) & 
@@ -15,21 +17,26 @@ parentaldata2 <- subset(parentaldata,
                         (!is.na(parentaldata$wp_offspring)) &
                         (!is.na(parentaldata$ep_offspring)))
 
+
 #create a new variable which is a binary for whether a species 
 
 parentaldata2$IncubationYN <- ifelse(parentaldata2$Incubation > 0, 1, 0)
+
 
 # Examine our data frame
 
 str(parentaldata2)
 
+
 #calculate the total number of offspring by adding within and extra-pair young
 
 parentaldata2$totaloffspring <- parentaldata2$ep_offspring + parentaldata2$wp_offspring
 
+
 #convert to have percentage of extra pair offspring
 
 parentaldata2$EPPnew <- parentaldata2$ep_offspring/parentaldata2$totaloffspring
+
 
 #scrutinize the data
 
@@ -37,7 +44,13 @@ hist(parentaldata2$EPPnew)
 max(parentaldata2$EPPnew)
 min(parentaldata2$EPPnew)
 
+
+#scale the explanatory variables
+
 parentaldata2$Incubationscaled <- scale(parentaldata2$Incubation)
+
+
+#glm model
 
 Parentalmodel <- glm(cbind(ep_offspring,wp_offspring) ~
                   Incubationscaled, family = binomial, data = parentaldata2)
